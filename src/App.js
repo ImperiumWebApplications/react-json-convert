5e4ad848d2d9c458de8ac39e1121c17cfefe763d
Aditya Singh <adityasingh220598@gmail.com>
2023-09-18 19:45:30 +0530

    Feature: JSON converter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { TextareaAutosize, Button } from "@mui/material";
 
-function App() {
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [outputValue, setOutputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleJsonConversion = () => {
+    try {
+      const unescapedJsonString = JSON.parse(inputValue);
+      const actualJsonObject = JSON.parse(unescapedJsonString);
+
+      setOutputValue(actualJsonObject);
+    } catch (error) {
+      setOutputValue("Invalid JSON format");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        padding: "20px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <TextareaAutosize
+        style={{ width: "100%", marginBottom: "20px" }}
+        minRows={20}
+        placeholder="Enter your unescaped string here."
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <Button
+        style={{ marginBottom: "20px" }}
+        variant="contained"
+        color="primary"
+        onClick={handleJsonConversion}
+      >
+        Convert to JSON
+      </Button>
+      <TextareaAutosize
+        style={{ width: "100%" }}
+        minRows={20}
+        value={outputValue}
+        readOnly
+      />
     </div>
   );
-}
+};
 
 export default App;
